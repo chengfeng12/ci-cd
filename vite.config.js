44150d1e772b769fb3975fb65b3eff40e1b7577a
YXR <913457727@qq.com>
2023-07-31 13:43:37 +0800

    update: oage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import path, { resolve } from "path"
 import vue from '@vitejs/plugin-vue'
-
+import { loadEnv } from "vite"
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default (configEnv) => {
+  const viteEnv = loadEnv(configEnv.mode, process.cwd())
+  const { VITE_PUBLIC_PATH } = viteEnv
+  return {
+    /** 打包时根据实际情况修改 base */
+    base: './',
+    /** Vite 插件 */
+    plugins: [
+      vue(),
+    ],
+
+  }
+}
+
