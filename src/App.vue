b6ef21d9c5ee2b194298a62a29b2c8891f754fcb
YXR <913457727@qq.com>
2023-07-31 16:03:08 +0800

    ci

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    test
+    这是一段文字
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
